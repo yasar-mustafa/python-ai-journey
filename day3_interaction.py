36a5e554b37e454d033957698a9a87439dba1546
Mustafa <mustafa_yasar@ymail.com>
2026-02-17 01:15:32 +0300

    Day 3 - user interaction practice

---
+++ day3_interaction.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+goal = input("What is your career goal? ")
+daily_hours = input("How many hours do you study daily? ")
+
+print("\n--- AI Journey Report ---")
+print(f"Hello {name}!")
+print(f"Your goal is to become a {goal}.")
+print(f"If you study {daily_hours} hours every day,")
+print("you are building a powerful future 🚀")
