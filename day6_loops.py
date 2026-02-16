f65d1868ed16e68516d16e0468db517e5596fbe3
Mustafa <mustafa_yasar@ymail.com>
2026-02-17 01:56:09 +0300

    Day 6 - loops practice

---
+++ day6_loops.py
@@ -0,0 +1,11 @@
+# Loops
+
+name = input("What is you name? ")
+days = int(input("How many days will you study? "))
+
+print("\n--- Study CountDown ---")
+
+for day in range(1,days + 1):  #range(start_value, stop_value)
+    print(f"Day {day} : {name} is building the AI future 🚀")
+
+print("\nConsistency Builds Success")
